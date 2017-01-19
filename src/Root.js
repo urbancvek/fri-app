e876261a61ec3f6d68dcbf0b14bd88b5f1faa3a8
Urban Cvek <urban.cvek@shopster.io>
2017-01-20 00:32:39 +0100

    Add mapbox-gl

--- src/Root.js
+++ src/Root.js
@@ -1,17 +1,39 @@
 // @flow
 import { autobind } from 'core-decorators';
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { StyleSheet } from 'react-native';
+import Mapbox, { MapView } from 'react-native-mapbox-gl';
+
+import { MAPBOX_TOKEN, MAPBOX_STYLE } from 'config/api';
+
+Mapbox.setAccessToken(MAPBOX_TOKEN);
 
 @autobind
 class Root extends Component {
   render() {
     return (
-      <View>
-        <Text>Root</Text>
-      </View>
+      <MapView
+        ref={(map: MapType) => this.map = map}
+        style={styles.container}
+
+        attributionButtonIsHidden
+        logoIsHidden
+        compassIsHidden
+        annotationsAreImmutable
+
+        styleURL={MAPBOX_STYLE}
+
+        zoomEnabled={false}
+        rotateEnabled
+      />
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
 export default Root;
---
+++ src/config/api.js
@@ -0,0 +1,8 @@
+// @flow
+const MAPBOX_STYLE = 'mapbox://styles/urbancvek/civ0qe4t401ib2hpbcmtrmnt6';
+const MAPBOX_TOKEN = 'pk.eyJ1IjoidXJiYW5jdmVrIiwiYSI6ImNpaXluZmJvOTAwM251YWx6eGRjaTB3aDAifQ.EJrh8YecJaD3mcxL_AzIDw';
+
+export {
+  MAPBOX_STYLE,
+  MAPBOX_TOKEN,
+};
