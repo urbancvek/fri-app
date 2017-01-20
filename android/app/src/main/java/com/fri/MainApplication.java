3d5412d7c23eff445d3ae26456a27c2a64ae7351
Urban Cvek <urban.cvek@shopster.io>
2017-01-20 20:21:32 +0100

    Add mapbox-gl for android

--- android/app/src/main/java/com/fri/MainApplication.java
+++ android/app/src/main/java/com/fri/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
+import com.mapbox.reactnativemapboxgl.ReactNativeMapboxGLPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -24,7 +25,8 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+        new MainReactPackage(),
+        new ReactNativeMapboxGLPackage()
       );
     }
   };
@@ -37,6 +39,6 @@ public class MainApplication extends Application implements ReactApplication {
   @Override
   public void onCreate() {
     super.onCreate();
-    SoLoader.init(this, /* native exopackage */ false);
+    SoLoader.init(this, false);
   }
 }
--- src/Root.js
+++ src/Root.js
@@ -1,7 +1,7 @@
 // @flow
 import { autobind } from 'core-decorators';
 import React, { Component } from 'react';
-import { NativeModules, NativeEventEmitter, View } from 'react-native';
+import { NativeModules, NativeEventEmitter, Platform, View } from 'react-native';
 import Mapbox from 'react-native-mapbox-gl';
 
 import { MAPBOX_TOKEN } from 'config/api';
@@ -26,7 +26,7 @@ class Root extends Component {
 
   componentDidMount() {
     EventEmitter.addListener('onLocationChange', this.onLocationChange);
-    IndoorLocation.startLocating();
+    if (Platform.OS === 'ios') IndoorLocation.startLocating();
   }
 
   onLocationChange(location: UserLocationType) {
