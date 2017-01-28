f5dde9f4e6810b0f32e7614b66809ddd34156df7
Urban Cvek <urban.cvek@shopster.io>
2017-01-28 19:31:01 +0100

    Complete event scene card, add html rendering for more rich content.

--- src/scenes/EventScene.js
+++ src/scenes/EventScene.js
@@ -1,8 +1,11 @@
 // @flow
 import React from 'react';
-import { View, Text, Image, ScrollView, TouchableOpacity } from 'react-native';
+import { View, Text, Image, ScrollView, TouchableOpacity, Dimensions } from 'react-native';
+import HTML from 'react-native-fence-html';
 
 import { StyleSheet } from 'standard';
+
+const { width } = Dimensions.get('window');
 
 const Person = ({ person }: PersonProps) => (
   <View style={personStyles.container}>
@@ -53,23 +56,59 @@ const personStyles = StyleSheet.create({
   },
 });
 
-const EventScene = ({ event }: Props, context) => (
+const html = `
+<div>
+  <img src="http://www-ti.fri.uni-lj.si/sites/default/files/patricio_buli%C4%87.png" width="190" height="240" />
+  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vestibulum lacus non nulla lacinia, id lobortis metus maximus. Maecenas finibus dui quam, iaculis volutpat purus ultrices non. Proin ut porttitor neque, sed bibendum velit.</p>
+</div>
+`;
+
+const renderers = {
+  img: (htmlAttribs, children, passProps) => {
+    const widthOfContent = width - 16 - 20;
+    const scale = widthOfContent / htmlAttribs.width;
+    const height = htmlAttribs.height * scale;
+
+    return (
+      <Image
+        source={{ uri: htmlAttribs.src, width: widthOfContent, height }}
+        style={passProps.htmlStyles.img}
+        {...passProps}
+      />
+    );
+  },
+};
+
+const EventScene = ({ event }: Props, { navigation }: Context) => (
   <View style={styles.container}>
     <View style={styles.header}>
       <View>
-        <Text style={[styles.room, { color: event.color }]}>
-          {event.room}
-        </Text>
+        <View style={styles.additionalInfo}>
+          <Text style={[styles.room, { color: event.color }]}>
+            {event.room}
+          </Text>
+          <Text style={styles.time}>
+            9:00 - 10:00
+          </Text>
+        </View>
         <Text style={styles.title}>
           {event.title.toUpperCase()}
         </Text>
       </View>
-      <TouchableOpacity onPress={() => context.navigation.popRoute()}>
-        <View style={styles.closeButton} />
+      <TouchableOpacity
+        onPress={() => navigation.popRoute()}
+        style={styles.closeButtonContainer}
+      >
+        <Image
+          source={require('assets/utils/close_button.png')}
+          style={styles.closeButton}
+        />
       </TouchableOpacity>
     </View>
 
-    <View>
+    <ScrollView
+      showsVerticalScrollIndicator={false}
+    >
       <Text style={styles.heading1}>
         PREDSTAVNIKI
       </Text>
@@ -87,10 +126,14 @@ const EventScene = ({ event }: Props, context) => (
       <Text style={styles.heading1}>
         PREDSTAVITEV
       </Text>
-      <Text style={styles.content}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vestibulum lacus non nulla lacinia, id lobortis metus maximus. Maecenas finibus dui quam, iaculis volutpat purus ultrices non. Proin ut porttitor neque, sed bibendum velit.
-      </Text>
-    </View>
+      <View style={styles.content}>
+        <HTML
+          html={html}
+          htmlStyles={htmlStyles}
+          renderers={renderers}
+        />
+      </View>
+    </ScrollView>
   </View>
 );
 
@@ -102,11 +145,33 @@ type Props = {
   event: EventType,
 };
 
+type Context = {
+  navigation: {
+    popRoute: () => void,
+  },
+};
+
+const htmlStyles = {
+  p: {
+    fontFamily: 'Montserrat',
+    fontSize: 13,
+    fontWeight: '300',
+    color: '#333333',
+    letterSpacing: 0.3,
+    lineHeight: 23,
+    marginTop: 0,
+    marginBottom: 0,
+  },
+  img: {
+    marginBottom: 15,
+  },
+};
+
 const styles = StyleSheet.create({
   container: {
     marginTop: 45,
-    marginHorizontal: 16,
-    marginBottom: 20,
+    marginHorizontal: 14,
+    marginBottom: 15,
     backgroundColor: 'white',
     paddingTop: 25,
     paddingHorizontal: 20,
@@ -116,15 +181,30 @@ const styles = StyleSheet.create({
   header: {
     flexDirection: 'row',
     justifyContent: 'space-between',
+    paddingBottom: 5,
+  },
+  closeButtonContainer: {
+    height: 35,
+    width: 35,
+    alignItems: 'flex-end',
   },
   closeButton: {
-    height: 20,
-    width: 20,
-    backgroundColor: 'green',
+    height: 25,
+    width: 25,
+  },
+  additionalInfo: {
+    flexDirection: 'row',
+    alignItems: 'center',
   },
   room: {
     fontWeight: 'Bold',
     fontSize: 18,
+    marginRight: 10,
+  },
+  time: {
+    fontWeight: 'Regular',
+    fontSize: 12,
+    color: '#9a9a9a',
   },
   title: {
     fontWeight: 'Bold',
@@ -142,11 +222,7 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
   },
   content: {
-    fontSize: 13,
-    fontWeight: 'Light',
-    color: '#333333',
-    letterSpacing: 0.3,
-    lineHeight: 23,
+    marginBottom: 30,
   },
 });
 
