d448a1ee9b95e574bbb2c215826d0c84717656ea
Urban Cvek <urban@cvek.me>
2017-11-16 17:44:05 +0100

    Update dependencies so that it works on new Xcode.

--- index.ios.js
+++ index.ios.js
@@ -1,15 +1,28 @@
 // @flow
-import React, { Component } from 'react';
-import { AppRegistry } from 'react-native';
+import { Navigation } from 'react-native-navigation';
+import registerScreens from 'screens';
 
-import Root from 'Root';
+const startApp = async () => {
+  Navigation.startTabBasedApp({
+    tabs: [{
+      label: 'Urnik',
+      screen: 'HomeTabScreen',
+      icon: require('assets/tab_icons/home_tab.png'),
+      selectedIcon: require('assets/tab_icons/home_tab_filled.png'),
+    },
+    {
+      label: 'Študij',
+      screen: 'StudyTabScreen',
+      icon: require('assets/tab_icons/study_tab.png'),
+      selectedIcon: require('assets/tab_icons/study_tab_filled.png'),
+    }],
+    tabsStyle: {
+      tabBarButtonColor: '#929292',
+      tabBarSelectedButtonColor: 'black',
+      tabBarBackgroundColor: 'white',
+    },
+  });
+};
 
-class FRI extends Component {
-  render() {
-    return (
-      <Root />
-    );
-  }
-}
-
-AppRegistry.registerComponent('FRI', () => FRI);
+registerScreens();
+startApp();
--- ios/FRI/AppDelegate.m
+++ ios/FRI/AppDelegate.m
@@ -12,31 +12,19 @@
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 
+#import "RCCManager.h"
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   NSURL *jsCodeLocation;
-
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
-
-  RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
-                                                      moduleName:@"FRI"
-                                               initialProperties:nil
-                                                   launchOptions:launchOptions];
-  rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
-
-  NSArray *objects = [[NSBundle mainBundle] loadNibNamed:@"LaunchScreen" owner:self options:nil];
-  UIImageView *loadingView = [[[objects objectAtIndex:0] subviews] objectAtIndex:0];
-  loadingView = [[UIImageView alloc] initWithImage:[loadingView image]];
-  loadingView.frame = [UIScreen mainScreen].bounds;
-  rootView.loadingView = loadingView;
-
+  
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
-  UIViewController *rootViewController = [UIViewController new];
-  rootViewController.view = rootView;
-  self.window.rootViewController = rootViewController;
-  [self.window makeKeyAndVisible];
+  self.window.backgroundColor = [UIColor whiteColor];
+  [[RCCManager sharedInstance] initBridgeWithBundleURL:jsCodeLocation launchOptions:launchOptions];
+
   return YES;
 }
 
